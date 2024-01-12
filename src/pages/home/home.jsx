25e00f1f1872383e4f5b4e21d7c418b58b892298
liuyy <yongyuanliu2022@outlook.com>
2024-01-12 10:35:05 +0800

    配置调整

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -2,9 +2,18 @@ import React from 'react';
 import { Card } from "antd"
 
 const Home = () => {
+    console.log('home init');
+    console.log("home page success");
     return (
         <Card>
             Hello World!
+            <br />
+            Hello Yongy
+            <br />
+            Hello X!
+            <br />
+            Hello G!
+            
         </Card>
     )
 }
