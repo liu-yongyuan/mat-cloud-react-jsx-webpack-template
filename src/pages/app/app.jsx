d5c7417bad88573c0a1f6890aac84e74d00772d1
liuyy <yongyuanliu2022@outlook.com>
2024-01-19 17:57:24 +0800

    首页改版，默认页面

--- src/pages/app/app.jsx
+++ src/pages/app/app.jsx
@@ -1,15 +1,80 @@
-import React from 'react';
-import { Layout } from 'antd';
-import { Outlet } from 'react-router-dom';
+import React, { useState } from 'react';
+import { PageContainer, ProCard, ProLayout, WaterMark } from '@ant-design/pro-components';
+import {
+    GithubFilled,
+    InfoCircleFilled,
+    QuestionCircleFilled,
+} from '@ant-design/icons';
+import defaultProps from './_defaultProps';
+
 import './app.less';
-const { Content } = Layout;
 const App = () => {
+    const [pathname, setPathname] = useState('/list/sub-page/sub-sub-page1');
     return (
-        <Layout className="mat-page-container mat-page-layout">
-            <Content className="mat-page-layout-content">
-                <Outlet />
-            </Content>
-        </Layout>
+        <WaterMark content="水印">
+            <div>
+                <ProLayout
+                    siderWidth={216}
+                    bgLayoutImgList={[
+                        {
+                            src: 'https://img.alicdn.com/imgextra/i2/O1CN01O4etvp1DvpFLKfuWq_!!6000000000279-2-tps-609-606.png',
+                            left: 85,
+                            bottom: 100,
+                            height: '303px',
+                        },
+                        {
+                            src: 'https://img.alicdn.com/imgextra/i2/O1CN01O4etvp1DvpFLKfuWq_!!6000000000279-2-tps-609-606.png',
+                            bottom: -68,
+                            right: -45,
+                            height: '303px',
+                        },
+                        {
+                            src: 'https://img.alicdn.com/imgextra/i3/O1CN018NxReL1shX85Yz6Cx_!!6000000005798-2-tps-884-496.png',
+                            bottom: 0,
+                            left: 0,
+                            width: '331px',
+                        },
+                    ]}
+                    {...defaultProps}
+                    location={{
+                        pathname,
+                    }}
+                    avatarProps={{
+                        src: 'https://gw.alipayobjects.com/zos/antfincdn/efFD%24IOql2/weixintupian_20170331104822.jpg',
+                        title: '七妮妮',
+                        size: 'small',
+                    }}
+                    actionsRender={(props) => {
+                        if (props.isMobile) return [];
+                        return [
+                            <InfoCircleFilled key="InfoCircleFilled" />,
+                            <QuestionCircleFilled key="QuestionCircleFilled" />,
+                            <GithubFilled key="GithubFilled" />,
+                        ];
+                    }}
+                    menuItemRender={(item, dom) => (
+                        <div
+                            onClick={() => {
+                                setPathname(item.path || '/welcome');
+                            }}
+                        >
+                            {dom}
+                        </div>
+                    )}
+                >
+                    <PageContainer>
+                        <ProCard
+                            style={{
+                                height: '100vh',
+                                minHeight: 800,
+                            }}
+                        >
+                            <div />
+                        </ProCard>
+                    </PageContainer>
+                </ProLayout>
+            </div>
+        </WaterMark>
     );
 }
 
--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -2,18 +2,9 @@ import React from 'react';
 import { Card } from "antd"
 
 const Home = () => {
-    console.log('home init');
-    console.log("home page success");
     return (
         <Card>
             Hello World!
-            <br />
-            Hello Yongy
-            <br />
-            Hello X!
-            <br />
-            Hello G!
-            
         </Card>
     )
 }
